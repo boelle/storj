46bf7e1fffa62905140281efbcc57f911ebe72a1
Ivan Fraixedes <ivan@fraixed.es>
2025-01-08 13:37:20 +0100

    WIP
    Change-Id: Ieab37add9a915a88371fe0096b5a2674c5fa3cf4

--- satellite/admin/back-office/devproxy/main.go
+++ satellite/admin/back-office/devproxy/main.go
@@ -4,55 +4,68 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
 	"net/url"
-	"os"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+
+	"storj.io/common/process"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "devproxy <proxy-port> <backend-url>",
+	Short: "Run a proxy for Satellite Admin Web App development to manage authorization",
+	Long: `"This command helps to work with the Satellite Admin Web App development.
+
+The Satellite Admin Web App requires a proxy for the users authorization. It is usually a Oauth2
+proxy.
+
+Dealing with a Oauth2 proxy on a development environment is tedious. This command runs a simple
+proxy that through a simple web interface allows to configure users and roles and through the proxy
+you can indicate the user to use with the "user" query parameter.
+
+<proxy-port> is the port where the proxy will listen.
+<backend-url> is the URL where the Satellite Admin Web App is listening
+(e.g. http://localhost:10005).
+`,
+	Args: cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx, _ := process.Ctx(cmd)
+		return runProxy(ctx, args[0], args[1])
+	},
+}
+
 func main() {
-	args, err := parseFlags()
+	process.ExecWithCustomOptions(rootCmd, process.ExecOptions{
+		LoadConfig: func(_ *cobra.Command, _ *viper.Viper) error {
+			return nil
+		},
+		LoggerFactory: func(logger *zap.Logger) *zap.Logger {
+			newLogger, level, err := process.NewLogger("placement-test")
+			if err != nil {
+				panic(err)
+			}
+			level.SetLevel(zap.DebugLevel)
+			return newLogger
+		},
+	})
+}
+
+func runProxy(ctx context.Context, proxyPort string, adminURL string) error {
+	port, err := strconv.ParseUint(proxyPort, 10, 16)
 	if err != nil {
-		exitWith(100, "invalid command line parameter. %s", err)
-	}
-}
-
-type params struct {
-	port       uint
-	backendURL *url.URL
-}
-
-func parseFlags() (params, error) {
-	fl := flag.NewFlagSet("", flag.ExitOnError)
-	fl.Usage = func() {
-		fmt.Printf(
-			"Run a proxy for satellite admin Web App development to be able to test different user's roles.\n\n",
-		)
-		fl.PrintDefaults()
+		return fmt.Errorf("failed to parse proxy port %s. %w", proxyPort, err)
 	}
 
-	var (
-		args = params{}
-		err  error
-	)
-
-	port := flag.Uint("port", 0, "the port where this proxy will listen")
-	backendURL := flag.String("backend-url", "", "absolute URL to the backend to proxy (e.g. http://localhost:10005)")
-
-	flag.Parse()
-
-	args.port = *port
-	args.backendURL, err = url.Parse(*backendURL)
+	backend, err := url.Parse(adminURL)
 	if err != nil {
-		return args, fmt.Errorf("invalid backend URL. %w", err)
+		return fmt.Errorf("failed to parse backend URL %s. %w", adminURL, err)
 	}
 
-	return args, nil
-}
-
-// exitWith prints to stderr the message formed by format and a appending a new line and exit the
-// program with code.
-func exitWith(code int, format string, a ...any) {
-	fmt.Fprintf(os.Stderr, format, a...)
-	os.Exit(code)
+	proxy := NewProxy(uint16(port), backend)
+	return proxy.Serve(ctx)
 }
--- satellite/admin/back-office/devproxy/proxy.go
+++ satellite/admin/back-office/devproxy/proxy.go
@@ -4,15 +4,55 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+
+	"github.com/zeebo/errs"
+
+	"storj.io/common/errs2"
 )
 
-func NewProxy(port uint, backend *url.URL) http.Server {
-	return http.Server{
-		Addr:    fmt.Sprintf("127.0.0.1:%d", port),
-		Handler: httputil.NewSingleHostReverseProxy(backend),
+// Proxy proxies requests to a back-end injecting the X-Forwarded-Groups header for the user
+// indicated by the "user" query parameter.
+type Proxy struct {
+	server http.Server
+
+	// users is a map of users and the groups they belong to.
+	users map[string][]string
+}
+
+// NewProxy creates a new Proxy instance listing on localhost on the provided port and proxying
+// the backend.
+func NewProxy(port uint16, backend *url.URL) Proxy {
+	return Proxy{
+		server: http.Server{
+			Addr:    fmt.Sprintf("127.0.0.1:%d", port),
+			Handler: httputil.NewSingleHostReverseProxy(backend),
+		},
+		users: make(map[string][]string),
 	}
+}
+
+func (p *Proxy) Serve(ctx context.Context) error {
+	group := errs2.Group{}
+
+	group.Go(func() error {
+		err := p.server.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			return nil
+		}
+
+		return err
+	})
+
+	group.Go(func() error {
+		<-ctx.Done()
+		return p.server.Shutdown(ctx)
+	})
+
+	return errs.Combine(group.Wait()...)
 }
